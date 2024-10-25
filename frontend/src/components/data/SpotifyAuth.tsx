cc46017c18973e5cd1f7bc31707965c278271df4
Colter Purcell <morrow165@outlook.com>
2024-10-25 08:56:44 -0700

    Add auth and auth refresh to spotify auth
    Went on a journey through the API docs and found that this is probably the best thing going for getting started. Will add more robust token treatment later. Adds both auth and auth refreshing functions without using useEffect. Retrieves name and profile pic on profile page if functioning properly.

---
+++ frontend/src/components/data/SpotifyAuth.tsx
@@ -0,0 +1,81 @@
+import { FunctionComponent } from 'react';
+import { Button } from '@mui/material';
+
+export const SpotifyLoginButton: FunctionComponent = () => {
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/login&scope=user-read-email%20user-read-private%20user-library-read%20user-library-modify`;
+
+  return (
+    <Button variant="contained" color={'primary'} href={AUTH_URL}>
+      Login with Spotify
+    </Button>
+  );
+};
+
+export const fetchAuthToken = async (code: string) => {
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: 'http://localhost:3000/login',
+      grant_type: 'authorization_code',
+    },
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization:
+        `Basic ` +
+        btoa(
+          process.env.REACT_APP_CLIENT_ID +
+            ':' +
+            process.env.REACT_APP_CLIENT_SECRET
+        ),
+    },
+    json: true,
+  };
+
+  try {
+    const response = await fetch(authOptions.url, {
+      method: 'POST',
+      headers: authOptions.headers,
+      body: new URLSearchParams(authOptions.form).toString(),
+    });
+    const data = await response.json();
+    window.localStorage.setItem('spotify_token', data.access_token);
+    window.localStorage.setItem('spotify_refresh_token', data.refresh_token);
+  } catch (error) {
+    console.error('Error fetching auth token', error);
+  }
+};
+
+export const getRefreshedToken = async (refreshToken: string) => {
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      refresh_token: refreshToken,
+      grant_type: 'refresh_token',
+    },
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization:
+        'Basic ' +
+        btoa(
+          process.env.REACT_APP_CLIENT_ID +
+            ':' +
+            process.env.REACT_APP_CLIENT_SECRET
+        ),
+    },
+    json: true,
+  };
+
+  try {
+    const response = await fetch(authOptions.url, {
+      method: 'POST',
+      headers: authOptions.headers,
+      body: new URLSearchParams(authOptions.form).toString(),
+    });
+    const data = await response.json();
+    window.localStorage.setItem('spotify_token', data.access_token);
+  } catch (error) {
+    console.error('Error fetching auth token', error);
+  }
+};
