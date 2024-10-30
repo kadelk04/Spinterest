9db099ece600e21eda4138e6d5de82cc54261a6c
kadelk04 <kadelk04@gmail.com>
2024-10-30 10:43:44 -0700

    removed references to home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import { Profile } from './components/pages/Profile';
 import { Login } from './components/pages/Login';
 import { Dashboard } from './components/pages/Dashboard';
-import { Home } from './components/pages/Home';
 import { Box, CssBaseline } from '@mui/material';
 import { Navbar } from './components/common/Navbar';
 import { Theme } from './components/common/Theme';
@@ -64,7 +63,6 @@ export default function App() {
             }}
           >
             <Routes>
-              <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/profile" element={<Profile />} />
